10eb219445313d076e08127f921ad993baf71284
GersonDG2 <gers.carri@gmail.com>
2024-08-09 22:02:52 -0600

    Primer commit

---
+++ Circunferencia.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * fue creado para calcular el area de una circunferencia
+ * @author Gerson Emilio Carrillo Martir
+ * @since 24-08-02
+ */
+
+public class Circunferencia {
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir al usuario que ingrese el radio de la circunferencia
+        System.out.print("Ingrese el radio de la circunferencia: ");
+        double radio = scanner.nextDouble();
+
+        // Calcular el área de la circunferencia
+        double area = calcularArea(radio);
+
+        // Mostrar el área de la circunferencia
+        System.out.println("El área de la circunferencia es: " + area);
+
+        // Cerrar el objeto Scanner
+        scanner.close();
+    }
+
+    // Método para calcular el área de una circunferencia dado su radio
+    public static double calcularArea(double radio) {
+        return Math.PI * Math.pow(radio, 2);
+    }
+}
